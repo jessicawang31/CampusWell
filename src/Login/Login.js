4f681ef2f3fd28498a60791b46c503cbeff92920
vortex421 <niconater0421@hotmail.com>
2024-05-14 17:15:10 -0700

    footer call

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from './NavBar'; 
+import { Footer } from "../components/Footer";
 
 export function Login() {
     return (
@@ -26,17 +27,7 @@ export function Login() {
                     </section>
                 </div>
             </main>
-            
-            <div className="introfootercontainer">
-                <div className="introfootercontent">
-                    <footer className="introfooter">
-                        <address>
-                        Contact us at <a href="mailto:nico0421@uw.edu">nico0421@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
-                        </address>
-                        <p>&copy; 2024 CampusWell.</p>
-                    </footer>
-                </div>
-            </div>
+            <Footer />
         </div>
     );
 }
