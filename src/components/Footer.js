7a048313119e2a823358f96f8a6086296f58ff51
vortex421 <niconater0421@hotmail.com>
2024-05-14 17:09:09 -0700

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export function Footer() {
 	return (
-        <div class="introfootercontainer">
-            <div class="introfootercontent">
-                <footer class="introfooter">
+        <div className="introfootercontainer">
+            <div className="introfootercontent">
+                <footer className="introfooter">
                     <address>
                     Contact us at <a href="mailto:nico0421@uw.edu">nico0421@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
                     </address>
