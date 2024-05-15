d24e12b1bb519dee7ce311db94a6a7c4795e1e79
vortex421 <niconater0421@hotmail.com>
2024-05-14 17:07:31 -0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Footer() {
+	return (
+        <div class="introfootercontainer">
+            <div class="introfootercontent">
+                <footer class="introfooter">
+                    <address>
+                    Contact us at <a href="mailto:nico0421@uw.edu">nico0421@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
+                    </address>
+                    <p>&copy; 2024 CampusWell.</p>
+                </footer>
+            </div>
+        </div>
+	);
+}
