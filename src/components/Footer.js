22c9de87d32991cd4cbef00b4e7ab7267e6353b3
Jessica Wang <jessicaw.040131@gmail.com>
2024-05-14 17:12:38 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export function Footer() {
             <div className="introfootercontent">
                 <footer className="introfooter">
                     <address>
-                    Contact us at <a href="mailto:nico0421@uw.edu">nico0421@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
+                    Contact us at <a href="mailto:campuswell@uw.edu">campuswell@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
                     </address>
                     <p>&copy; 2024 CampusWell.</p>
                 </footer>
