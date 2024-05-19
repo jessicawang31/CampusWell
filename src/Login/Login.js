0e20d0758adbee46d2507d60b2832de6a376ee5e
vortex421 <niconater0421@hotmail.com>
2024-05-19 01:50:14 -0700

    footercss

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/Footer.js";
 import './Login.css';
 import '../index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Login() {
     return (
@@ -15,9 +16,9 @@ export default function Login() {
                         <h2 className="introh2">Log in to CampusWell</h2>
                         <h2 className="text-small introh2">Don't have an account? <a href="https://example.com/">Sign Up!</a></h2>
                         <form>
-                            <div className="entryboxes">
+                            <div className="entryboxes py-4">
                                 <label for="email_input" className="titles">
-                                    <b>Email:</b><br />
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Email:</b><br />
                                 </label><input id="email_input" type="email" name="email" placeholder="email@domain.com" /><br />
                                 <label for="password_input" className="titles">
                                     <b>Password:</b><br />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-	return (
-        <div className="introfootercontainer">
-            <div className="introfootercontent">
-                <footer className="introfooter">
-                    <address>
-                    Contact us at <a href="mailto:campuswell@uw.edu">campuswell@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
-                    </address>
-                    <p>&copy; 2024 CampusWell.</p>
-                </footer>
+    return (
+        <div>
+            <div className="introfootercontainer footer mt-auto fixed-bottom">
+                <div className="introfootercontent">
+                    <footer className="introfooter container-fluid">
+                        <div className="row">
+                            <div className="col text-start">
+                                <p>&copy; 2024 CampusWell.</p>
+                            </div>
+                            <div className="col text-center">
+                                <address>
+                                    Contact us at <a href="mailto:campuswell@uw.edu">campuswell@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
+                                </address>
+                            </div>
+                            <div className="col text-end">
+                                <a href="/privacy-policy" className="me-3">Privacy Policy</a>
+                                <a href="/terms-of-service" className="me-3">Terms of Service</a>
+                                <a href="/about-us">About Us</a>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
             </div>
         </div>
-	);
+    );
 }
