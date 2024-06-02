e8f2c23f2481f40488c2112f3c7334e5a799b848
ntab-spec <ntabera@uw.edu>
2024-06-01 17:43:21 -0700

    footer/pa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Footer() {
     return (
-        <footer className="footer mt-auto py-3">
-            <div className="container-fluid text-light">
+        <footer className="introfootercontainer footer sticky-bottom">
+            <div className="introfootercontent container-fluid">
                 <div className="row">
                     <div className="col text-start">
                         <p>&copy; 2024 CampusWell.</p>
                     </div>
                     <div className="col text-center">
                         <address>
-                            Contact us at <a href="mailto:campuswell@uw.edu" className="text-light">campuswell@uw.edu</a>, or at <a href="tel:XXXXXXXXXX" className="text-light">(XXXXXXXXXX)</a>.
+                            Contact us at <a href="mailto:campuswell@uw.edu">campuswell@uw.edu</a>, or at <a href="tel:XXXXXXXXXX">(XXXXXXXXXX)</a>.
                         </address>
                     </div>
                     <div className="col text-end">
-                        <a href="/privacy-policy" className="text-light me-3">Privacy Policy</a>
-                        <a href="/terms-of-service" className="text-light me-3">Terms of Service</a>
-                        <a href="/about-us" className="text-light">About Us</a>
+                        <a href="/privacy-policy" className="me-3">Privacy Policy</a>
+                        <a href="/terms-of-service" className="me-3">Terms of Service</a>
+                        <a href="/about-us">About Us</a>
                     </div>
                 </div>
             </div>
