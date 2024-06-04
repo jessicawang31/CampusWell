a5441357fefb75738ebf80d7657b62f054163117
Jessica Wang <jessicaw.040131@gmail.com>
2024-06-03 20:09:25 -0700

    login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
+<<<<<<< Updated upstream
     return (
         <div className="introflex vh-100">
             <header className="introheader">
@@ -18,35 +19,50 @@ export default function Login() {
                             <div className="">
                                 <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                                 <p className="text-black-50 mb-5">Please enter your login and password!</p>
+=======
+    return (
+        <div className="introflex vh-100">
+            <header className="introheader">
+                <h1 className="introh1">CampusWell</h1>
+            </header>
+            <main className="d-flex justify-content-center align-items-center flex-grow-1">
+                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div className="card text-black bg-white" style={{ borderRadius: '1rem' }}>
+                        <div className="card-body text-center">
+                            <div className="">
+                                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                <p className="text-black-50 mb-5">Please enter your login and password!</p>
+>>>>>>> Stashed changes
 
-                                <div className="form-outline form-black mb-4">
-                                    <input type="email" id="typeEmailX" className="form-control form-control-lg" placeholder="email@domain.com" />
-                                    <label className="form-label" htmlFor="typeEmailX">Email</label>
-                                </div>
+                                <div className="form-outline form-black mb-4">
+                                    <input type="email" id="typeEmailX" className="form-control form-control-lg" placeholder="email@domain.com" />
+                                    <label className="form-label" htmlFor="typeEmailX">Email</label>
+                                </div>
 
-                                <div className="form-outline form-black mb-4">
-                                    <input type="password" id="typePasswordX" className="form-control form-control-lg" placeholder="******" />
-                                    <label className="form-label" htmlFor="typePasswordX">Password</label>
-                                </div>
+                                <div className="form-outline form-black mb-4">
+                                    <input type="password" id="typePasswordX" className="form-control form-control-lg" placeholder="******" />
+                                    <label className="form-label" htmlFor="typePasswordX">Password</label>
+                                </div>
 
-                                <Link to="/main"><button className="btn btn-outline-dark btn-lg px-5" type="button">Login</button></Link>
+                                <Link to="/main"><button className="btn btn-outline-dark btn-lg px-5" type="button">Login</button></Link>
 
-                                <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                                    <a href="#!" className="text-black"><i className="fab fa-facebook-f fa-lg"></i></a>
-                                    <a href="#!" className="text-black"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                    <a href="#!" className="text-black"><i className="fab fa-google fa-lg"></i></a>
-                                </div>
+                                <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                                    <a href="#!" className="text-black"><i className="fab fa-facebook-f fa-lg"></i></a>
+                                    <a href="#!" className="text-black"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                    <a href="#!" className="text-black"><i className="fab fa-google fa-lg"></i></a>
+                                </div>
 
-                            </div>
+                            </div>
 
-                            <div>
-                                <p className="mb-0">Don't have an account? <a href="ProfileAccount.js"><Link to="/account">Sign Up!</Link></a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
+                            <div>
+                                <p className="mb-0">Don't have an account? <a href="ProfileAccount.js"><Link to="/account">Sign Up!</Link></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
 
+<<<<<<< Updated upstream
             <footer>
                 <Footer />
             </footer>
@@ -54,3 +70,12 @@ export default function Login() {
         </div>
     );
 }
+=======
+            <footer>
+                <Footer />
+            </footer>
+        
+        </div>
+    );
+}
+>>>>>>> Stashed changes
