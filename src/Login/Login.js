1c1a9b0a4cfda9491423e432d0602cd4526c0957
Jessica Wang <jessicaw.040131@gmail.com>
2024-06-03 21:01:14 -0700

    login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -31,12 +31,6 @@ export default function Login() {
 
                                 <Link to="/main"><button className="btn btn-outline-dark btn-lg px-5" type="button">Login</button></Link>
 
-                                <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                                    <a href="#!" className="text-black"><i className="fab fa-facebook-f fa-lg"></i></a>
-                                    <a href="#!" className="text-black"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                    <a href="#!" className="text-black"><i className="fab fa-google fa-lg"></i></a>
-                                </div>
-
                             </div>
 
                             <div>
